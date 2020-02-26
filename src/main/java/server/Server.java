c88449ed0eb1a7d7d96a9ad2b5e3990a91abc8ea
EUDALD ELIAS VILANOVA <eeliasvi8.alumnes@ub052229.mat.ub.edu>
2020-02-26 17:27:45 +0100

    serverSide

---
+++ src/main/java/server/Server.java
@@ -0,0 +1,39 @@
+package server;
+
+import java.net.ServerSocket;
+
+class Server{
+    
+    ServerSocket serverSocket;
+    public static int MAX_CONNECTIONS = 50;
+    
+    
+    public Server(){
+        try{
+            serverSocket = new ServerSocket( 80, MAX_CONNECTIONS );
+        }catch(Exception e{
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+    
+    public static void main(String[] args){
+        new Server().run;
+    }
+    
+    public void run(){
+        while(true){
+            try{
+                Socket client = ss.accept();
+                createNewGame(client);
+            }catch(Exception e){
+                //TODO
+                continue;
+            }
+        }
+    }
+    
+    private createNewGame(Socket client){
+        Thread nt = new Thread(new ServerEngine());
+        nt.start();
+    }
+}
